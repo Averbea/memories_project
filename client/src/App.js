28818393fc2d5d03ea256384c9aa78f38643bd10
Averbea <alexave99@gmail.com>
2022-05-03 19:53:14 +0200

    fix eslint and move navbar to component

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,31 @@
-import React from "react";
-import {Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import { useDispatch } from "react-redux";
+import React from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
-import {getPosts} from './actions/posts'
-import Posts from './components/Posts/Posts'
-import Form from "./components/Form/Form";
-import memories from './images/memories.png'
-import useStyles from './styles'
+import { getPosts } from './actions/posts';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
+import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar.js';
 
 export default function App() {
-  const [currentId, setCurrentId] = React.useState(null)
+  const [currentId, setCurrentId] = React.useState(null);
   const classes = useStyles();
   const dispatch = useDispatch();
 
-  React.useEffect(() =>{
-    console.log("dispatching get Posts")
+  React.useEffect(() => {
     dispatch(getPosts());
-  },[dispatch])
-  
+  }, [dispatch]);
+
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
-      </AppBar>
+      <Navbar> </Navbar>
       <Grow in>
         <Container>
           <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts currentId={currentId} setCurrentId={setCurrentId}/>
+              <Posts currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
@@ -40,7 +36,3 @@ export default function App() {
     </Container>
   );
 }
-
-
-
-
