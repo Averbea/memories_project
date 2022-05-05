25710ca7ffea81a484a569446b8bee9eabf02fcc
Averbea <alexave99@gmail.com>
2022-05-05 21:20:59 +0200

    ESLint fix and updating react-scripts version because of bug

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,19 +1,18 @@
-import Posts from "../components/Posts/Posts";
-import { DELETE, UPDATE, LIKE, FETCH_ALL, CREATE } from "../constants/actionTypes";
 
-export default (posts_state = [], action) => {
-    switch(action.type){
-        case DELETE:
-            return posts_state.filter(post => post._id !== action.payload) 
-        case UPDATE:
-        case LIKE:
-            return posts_state.map((post) => post._id == action.payload._id? action.payload: post )
-        case FETCH_ALL:
-            return action.payload;
-        case CREATE:
-            return [...posts_state, action.payload];
-        default: 
-            return posts_state;
-    }
+import { DELETE, UPDATE, LIKE, FETCH_ALL, CREATE } from '../constants/actionTypes';
 
-}
+export default (postsState = [], action) => {
+  switch (action.type) {
+    case DELETE:
+      return postsState.filter((post) => post._id !== action.payload);
+    case UPDATE:
+    case LIKE:
+      return postsState.map((post) => (post._id === action.payload._id ? action.payload : post));
+    case FETCH_ALL:
+      return action.payload;
+    case CREATE:
+      return [...postsState, action.payload];
+    default:
+      return postsState;
+  }
+};
